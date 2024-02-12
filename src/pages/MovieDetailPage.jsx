6f6e59eba6e664bc3eb038cd8c8d885b7db9f9e5
dvkhangIT <dvkhang2100321@student.ctuet.edu.vn>
2024-02-12 08:23:02 +0700

    movie detail page fetching casts
    Signed-off-by: dvkhangIT <dvkhang2100321@student.ctuet.edu.vn>

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -9,8 +9,6 @@ const MovieDetailPage = () => {
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`,
     fetcher
   );
-  //   const movies = data?.results || [];
-  console.log(data);
   if (!data) return null;
   const { backdrop_path, poster_path, title, genres, overview } = data;
   return (
@@ -30,7 +28,7 @@ const MovieDetailPage = () => {
           className="w-full h-full object-cover rounded-xl"
         />
       </div>
-      <h1 className="text-center font-bold mb-10 text-3xl text-white">{title}</h1>
+      <h1 className="text-center font-bold mb-10 text-4xl text-white">{title}</h1>
       {genres.length > 0 && (
         <div className="flex items-center justify-center gap-x-10 mb-10">
           {genres.map((item) => (
@@ -43,9 +41,52 @@ const MovieDetailPage = () => {
           ))}
         </div>
       )}
-      <div className="text-center leading-relaxed max-w-[600px] mx-auto">{overview}</div>
+      <p className="text-center leading-relaxed max-w-[600px] mx-auto mb-10">
+        {overview}
+      </p>
+      <MovieCredits></MovieCredits>
+      <MovieVideos></MovieVideos>
     </div>
   );
 };
-
+function MovieCredits() {
+  // https://api.themoviedb.org/3/movie/{movie_id}/credits
+  const { movieId } = useParams();
+  const { data, error } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`,
+    fetcher
+  );
+  if (!data) return null;
+  const { cast } = data;
+  if (!cast || cast.length <= 0) return null;
+  return (
+    <div className="py-10">
+      <h2 className="capitalize text-center text-3xl mb-10">casts</h2>
+      <div className="grid grid-cols-4 gap-5">
+        {cast.slice(0, 4).map((item) => (
+          <div className="cast-item" key={item.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/original/${item.profile_path}`}
+              className="w-full object-cover h-[350px] rounded-lg mb-3"
+            />
+            <h3 className="text-xl font-medium">{item.name}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+function MovieVideos() {
+  const { movieId } = useParams();
+  const { data, error } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`,
+    fetcher
+  );
+  if (!data) return null;
+  console.log(`MovieVideos ~ data:`, data);
+  return <div></div>;
+}
+{
+  /* <iframe width="935" height="526" src="https://www.youtube.com/embed/G82CgMekC5U" title="Badland Hunters | Inside Look | Netflix [ENG SUB]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */
+}
 export default MovieDetailPage;
